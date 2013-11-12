f8e2b34cc66ad92095be30c0db1b779593262933
--global <--global>
2013-11-12 14:07:41 +0100

    Alpha stage

--- src/ui/Canvas.java
+++ src/ui/Canvas.java
@@ -4,10 +4,25 @@
  */
 package ui;
 
+//imports
+
+import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.Rectangle;
+import java.util.List;
+import ui.Figures.Square;
+import javax.swing.*;
+
 /**
  *
  * @author johan
  */
-public class Canvas {
+public class Canvas extends JFrame {
+    public Canvas(){
+        super("Maze");
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        Square squares = new Square();
+    }
     
 }
